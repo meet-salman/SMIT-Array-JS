0102e2d987a7ad4b6a57f567a9cb3767402b2477
Salman Ahmed <say2salmanahmed@gmail.com>
2023-08-25 09:23:39 +0500

    Arrays

---
+++ app.js
@@ -0,0 +1,149 @@
+// let num = [1, 2, 3, 4, 5]
+// console.log(num);
+
+
+// num1 = num[3];
+// console.log(num1);
+
+
+
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+
+
+
+// let fruits = ['kela', 'kharbooza', 'nashpati', 'kinnu', 'mittha', 'angoor']
+// console.log(fruits);
+
+// fruits.push('leache')
+// console.log(fruits);
+
+
+// fruits.pop()
+// console.log(fruits);
+
+
+// fruits.unshift('papeeta')
+// console.log(fruits);
+
+
+// fruits.shift('papeeta')
+// console.log(fruits);
+
+
+// fruits.unshift('papeeta')
+// console.log(fruits);
+
+
+// let eaten = fruits.slice(1 , 3)
+// console.log(eaten);
+
+
+// fruits.splice(1, 3, 'tarbooz', 'garma')
+// console.log(fruits);
+
+
+
+// let sentence = 'My Name Is Salman Ahmed';
+// let names = sentence.split(' ');
+// console.log(names);
+
+// names[3] = "Shaheer";
+// console.log(names);
+
+
+
+// let words = ['My', 'Name', 'Is', 'Salman', 'Ahmed'];
+// let joinWords = words.join(' ');
+// console.log(joinWords);
+
+// words[3] = 'Shaheer';
+// let change = words.join(' ');
+// console.log(change);
+
+
+
+
+
+// let num2 = [1, 2, 3];
+// let num3 = [4,  5, 6];
+// let newNum = num2.concat(num3);
+// console.log(newNum);
+
+
+// newNum.reverse();
+// console.log(newNum);
+
+
+
+// let numbers = [5, 6, 2, 9, 1, 3]
+// // console.log(numbers);
+// // numbers.sort();
+// // console.log(numbers);
+
+
+
+// const indexNum = numbers.indexOf(9);
+// console.log(indexNum);
+
+
+const first = document.querySelector('.fruits1');
+const second = document.querySelector('.fruits2');
+const third = document.querySelector('.fruits3');
+const fourth = document.querySelector('.fruits4');
+const fifth = document.querySelector('.fruits5');
+const sixth = document.querySelector('.fruits6');
+const seventh = document.querySelector('.fruits7');
+const eight = document.querySelector('.fruits8');
+
+let fruits = ['Mango', ' Banana', ' Graps', ' Apple', ' Orange', ' Watermelon', ' Pinapple', ' Cherry'];
+
+
+// fruits.push(' Nashpati ', ' Garma');
+// first.innerHTML = fruits;
+
+// fruits.pop();
+// second.innerHTML = fruits;
+
+// fruits.shift();
+// third.innerHTML = fruits;
+
+// fruits.unshift('Timatar');
+// fourth.innerHTML = fruits;
+
+// fruits.splice(3, 2, ' Jaha se no start waha se end no tk delete ');
+// fifth.innerHTML = fruits;
+
+let gfdf = fruits.slice(3, 5);
+sixth.innerHTML = gfdf;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
